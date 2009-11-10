93fafb4151ddf38aea58e18acefd6955a6bf23c5
SueBlack <shamanic.sue@gmail.com>
2009-11-10 15:56:40 +0100

    * Unit-Hittest req update
    Signed-off-by: SueBlack <shamanic.sue@gmail.com>

--- src/WorldOfAnguis/Graphics/DirectX/DirectXInterface.cpp
+++ src/WorldOfAnguis/Graphics/DirectX/DirectXInterface.cpp
@@ -8,7 +8,10 @@
  *                    World Of Anguis
  *
  */
-// komment //#pragma comment(lib, "d3d9.lib")#ifdef _DEBUG
+
+
+#pragma comment(lib, "d3d9.lib")
+#ifdef _DEBUG
 #pragma comment(lib, "d3dx9d.lib")
 #else
 #pragma comment(lib, "d3dx9.lib")
--- src/WorldOfAnguis/Units/Unit.cpp
+++ src/WorldOfAnguis/Units/Unit.cpp
@@ -36,30 +36,38 @@ bool Unit::Update()
 	int PPHM = world->GetPixelPerHitMap();
 	int MapWidth = world->GetHitMapWidth();
 	
-	int dX,dY;
-	for(dX=X;dX!=X+vX;vX>0?dX++:dX--)		// increase or decrease the dX depending on we are moving left or right //
-		{
-		if(dX<0) {dX = 0; vX = 0; break;}
-		else if(dX+Width>world->GetWidth()) {dX = world->GetWidth()-Width; vX = 0; break;}
+	int dX,dY; //new coord.
 
-		if(Map[((Y/PPHM)*MapWidth)+(dX/PPHM)] > 0)				// Collision along the X axis //
-			{
-			vX = 0;
-			break;
-			}
-		}
-	for(dY=Y;dY!=Y+vY;vY>0?dY++:dY--)		// increase or decrease the dY depending on we are moving up or down //
-		{
-		if(dY<0) {dY = 0; vY = 0; break;}
-		else if(dY+Height>world->GetHeight()) {dY = world->GetHeight()-Height; vY = 0; break;}
 
-		if(Map[((dY/PPHM)*MapWidth)+(dX/PPHM)] > 0)
-			{
-			vY = 0;		// Hit the ground -> lost our Y velocity //
-			break;
-			}
-		}
+	/* kell egy külön ütközésvizsgálat, csak 1 pontra */
 
+
+	//for(dX=X;dX!=X+vX;vX>0?dX++:dX--)		// increase or decrease the dX depending on we are moving left or right //
+	//	{
+	//	if(dX<0) {dX = 0; vX = 0; break;}
+	//	else if(dX+Width>world->GetWidth()) {dX = world->GetWidth()-Width; vX = 0; break;}
+
+	//	if(Map[((Y/PPHM)*MapWidth)+(dX/PPHM)] > 0)				// Collision along the X axis //
+	//		{
+	//		vX = 0;
+	//		Collided = true;
+	//		break;
+	//		}
+	//	}
+
+	//for(dY=Y;dY!=Y+vY;vY>0?dY++:dY--)		// increase or decrease the dY depending on we are moving up or down //
+	//	{
+	//	if(dY<0) {dY = 0; vY = 0; break;}
+	//	else if(dY+Height>world->GetHeight()) {dY = world->GetHeight()-Height; vY = 0; break;}
+
+	//	if(Map[((dY/PPHM)*MapWidth)+(dX/PPHM)] > 0)
+	//		{
+	//		vY = 0;		// Hit the ground -> lost our Y velocity //
+	//		Collided = true;
+	//		break;
+	//		}
+	//	}
+	
 	X = dX;
 	Y = dY;
 return Collided;
